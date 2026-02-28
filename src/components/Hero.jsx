918e8f44136e8c5bb74e68c3e7416161ffc08596
Ahmad Ro'in Fannani <v.ahmad.fannani@adira.co.id>
2026-02-28 18:20:11 +0700

    update web adira english description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,58 +1,65 @@
-import { HERO_CONTENT } from "../constants"
-import profilePic from "../assets/myprofile.jpg"
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/myprofile.jpg";
 import { motion } from "motion/react";
 
 const container = (delay) => ({
-    hidden: {x: -100, opacity: 0},
-    visible: {
-        x: 0, 
-        opacity: 1, 
-        transition: {duration: 0.5, delay: delay},
-    } 
-})
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 
-const Hero = ({language}) => {
+const Hero = ({ language }) => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35 pt-28">
-        <div className="flex flex-wrap md:justify-center sm:justify-center">
-            <div className="w-full lg:w-1/2">
-                <div className="flex flex-col items-center lg:items-start">
-                    <motion.h1
-                    variants={container(0)}
-                    initial="hidden"
-                    animate="visible"
-                    className='pb-16 text-6xl font-extralight tracking-tight lg:mt-16 lg:text-8xl'>
-                        Ahmad Ro'in Fannani
-                    </motion.h1>
-                    <motion.span 
-                    variants={container(0.5)}
-                    initial="hidden"
-                    animate="visible"
-                    className='bg-gradient-to-r from-sky-300 via-slate-500 to-blue-500 bg-clip-text text-3xl tracking-light text-transparent'>
-                        {language === 'en' ? 'Web Developer' : 'Pengembang Web'}
-                    </motion.span>
-                    <motion.p 
-                    variants={container(1)}
-                    initial="hidden"
-                    animate="visible"
-                    className='text-justify my-2 max-w-xl motion.py-6 font-light tracking-tighter'>{language === 'en' ? HERO_CONTENT.en : HERO_CONTENT.id}</motion.p>
-                </div>
-            </div>
-            <div className="mt-2 lg:w-1/2 lg:p-8">
-                <div className="flex justify-center">
-                    <motion.img
-                    className='rounded-2xl'
-                    initial={{ x: 100, opacity: 0 }}
-                    animate={{ x: 0, opacity: 1}}
-                    transition={{ duration: 1, delay: 0.5 }}
-                    src={profilePic} alt="Profile" 
-                    width={500}
-                    height={500}/>
-                </div>
-            </div>
+      <div className="flex flex-wrap md:justify-center sm:justify-center">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-extralight tracking-tight lg:mt-16 lg:text-8xl"
+            >
+              Ahmad Ro'in Fannani
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-sky-300 via-slate-500 to-blue-500 bg-clip-text text-3xl tracking-light text-transparent"
+            >
+              {language === "en" ? "Web Developer" : "Pengembang Web"}
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="text-justify my-2 max-w-xl motion.py-6 font-light tracking-tighter"
+            >
+              {language === "en" ? HERO_CONTENT.en : HERO_CONTENT.id}
+            </motion.p>
+          </div>
         </div>
+        <div className="mt-2 lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <motion.img
+              className="rounded-2xl"
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 0.5 }}
+              src={profilePic}
+              alt="Profile"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
-import project1 from '../assets/projects/mockup_ecommerce.png';
-import project2 from '../assets/projects/mockup_beasiswa.png';
-import project3 from '../assets/projects/mockup_portfolio.png';
-import WebAdira from '../assets/projects/WebAdira.jpeg';
+import project1 from "../assets/projects/mockup_ecommerce.png";
+import project2 from "../assets/projects/mockup_beasiswa.png";
+import project3 from "../assets/projects/mockup_portfolio.png";
+import WebAdira from "../assets/projects/WebAdira.jpeg";
 
 export const HERO_CONTENT = {
   en: `Hi, I am Ahmad Ro'in Fannani, a final year student currently pursuing Information Systems studies. I have an interest in web development, focusing on building systems that provide effective business solutions. I have experience working on web development projects as a full-stack and front-end developer. I enjoy working in teams, leveraging my adaptability, communication skills, and team management abilities gained from my organizational experiences.`,
@@ -16,20 +16,20 @@ export const ABOUT_TEXT = {
 export const INTERNSHIP = {
   en: [
     {
-      year: 'March 2025 - September 2025',
-      role: 'Frontend Web Developer',
-      company: 'PT. Adira Dinamika Multi Finance',
+      year: "March 2025 - September 2025",
+      role: "Frontend Web Developer",
+      company: "PT. Adira Dinamika Multi Finance",
       description:
-        'During my internship as a Frontend Web Developer at PT Adira Dinamika Multi Finance Tbk (Adira Finance), I was responsible for developing and optimizing the company’s website landing pages and content management system (CMS). I implemented responsive and reusable UI components, integrated APIs, and ensured seamless user experience across devices. The project was successfully deployed and went live on https://www.adira.co.id, contributing directly to the company’s official digital platform.',
+        "During my internship as a Frontend Web Developer at PT Adira Dinamika Multi Finance Tbk (Adira Finance), I was responsible for developing and optimizing the company’s website landing pages and content management system (CMS). I implemented responsive and reusable UI components, integrated APIs, and ensured seamless user experience across devices. The project was successfully deployed and went live on https://www.adira.co.id, contributing directly to the company’s official digital platform.",
     },
   ],
   id: [
     {
-      year: 'Maret 2025 - September 2025',
-      role: 'Frontend Web Developer',
-      company: 'PT. Adira Dinamika Multi Finance',
+      year: "Maret 2025 - September 2025",
+      role: "Frontend Web Developer",
+      company: "PT. Adira Dinamika Multi Finance",
       description:
-        'Selama masa magang saya sebagai Frontend Web Developer di PT Adira Dinamika Multi Finance Tbk (Adira Finance), saya bertanggung jawab untuk mengembangkan dan mengoptimalkan landing page dan sistem manajemen konten (CMS) situs web perusahaan. Saya mengimplementasikan komponen UI yang responsif dan dapat digunakan kembali, mengintegrasikan API, dan memastikan pengalaman pengguna yang lancar di berbagai perangkat. Proyek ini berhasil diimplementasikan dan ditayangkan di https://www.adira.co.id, yang secara langsung berkontribusi pada platform digital resmi perusahaan.',
+        "Selama masa magang saya sebagai Frontend Web Developer di PT Adira Dinamika Multi Finance Tbk (Adira Finance), saya bertanggung jawab untuk mengembangkan dan mengoptimalkan landing page dan sistem manajemen konten (CMS) situs web perusahaan. Saya mengimplementasikan komponen UI yang responsif dan dapat digunakan kembali, mengintegrasikan API, dan memastikan pengalaman pengguna yang lancar di berbagai perangkat. Proyek ini berhasil diimplementasikan dan ditayangkan di https://www.adira.co.id, yang secara langsung berkontribusi pada platform digital resmi perusahaan.",
     },
   ],
 };
@@ -37,20 +37,20 @@ export const INTERNSHIP = {
 export const COMPETITION = {
   en: [
     {
-      year: 'April 2025 - November 2025',
-      competiton: 'Pekan Ilmiah Mahasiswa Nasional 2025',
-      event: 'Program Kreativitas Mahasiswa',
+      year: "April 2025 - November 2025",
+      competiton: "Pekan Ilmiah Mahasiswa Nasional 2025",
+      event: "Program Kreativitas Mahasiswa",
       description:
-        'As a participant in Pekan Ilmiah Mahasiswa Nasional 2025, organized by the Kementerian Pendidikan Tinggi, Sains, dan Teknologi Republik Indonesia, I earned the opportunity to compete after successfully completing the Program Kreativitas Mahasiswa Karsa Cipta 2025. Together with my team, we developed a smart glasses prototype designed to assist visually impaired individuals in their daily activities. Our dedication and collaborative effort led us to achieve 3rd place nationally (Bronze Medal equivalent) in the PKM-KC Poster Category, marking a significant milestone in our academic and innovation journey.',
+        "As a participant in Pekan Ilmiah Mahasiswa Nasional 2025, organized by the Kementerian Pendidikan Tinggi, Sains, dan Teknologi Republik Indonesia, I earned the opportunity to compete after successfully completing the Program Kreativitas Mahasiswa Karsa Cipta 2025. Together with my team, we developed a smart glasses prototype designed to assist visually impaired individuals in their daily activities. Our dedication and collaborative effort led us to achieve 3rd place nationally (Bronze Medal equivalent) in the PKM-KC Poster Category, marking a significant milestone in our academic and innovation journey.",
     },
   ],
   id: [
     {
-      year: 'April 2025 - November 2025',
-      competiton: 'Pekan Ilmiah Mahasiswa Nasional 2025',
-      event: 'Program Kreativitas Mahasiswa',
+      year: "April 2025 - November 2025",
+      competiton: "Pekan Ilmiah Mahasiswa Nasional 2025",
+      event: "Program Kreativitas Mahasiswa",
       description:
-        'Sebagai peserta Pekan Ilmiah Mahasiswa Nasional 2025, yang diselenggarakan oleh Kementerian Pendidikan Tinggi, Sains, dan Teknologi Republik Indonesia, saya mendapatkan kesempatan untuk berkompetisi setelah berhasil menyelesaikan Program Kreativitas Mahasiswa Karsa Cipta 2025. Bersama tim saya, kami mengembangkan prototipe kacamata pintar yang dirancang untuk membantu penyandang tunanetra dalam aktivitas sehari-hari mereka. Dedikasi dan kerja sama kami membawa kami meraih juara 3 nasional (setara Medali Perunggu) dalam Kategori Poster PKM-KC, yang menandai tonggak penting dalam perjalanan akademis dan inovasi kami.',
+        "Sebagai peserta Pekan Ilmiah Mahasiswa Nasional 2025, yang diselenggarakan oleh Kementerian Pendidikan Tinggi, Sains, dan Teknologi Republik Indonesia, saya mendapatkan kesempatan untuk berkompetisi setelah berhasil menyelesaikan Program Kreativitas Mahasiswa Karsa Cipta 2025. Bersama tim saya, kami mengembangkan prototipe kacamata pintar yang dirancang untuk membantu penyandang tunanetra dalam aktivitas sehari-hari mereka. Dedikasi dan kerja sama kami membawa kami meraih juara 3 nasional (setara Medali Perunggu) dalam Kategori Poster PKM-KC, yang menandai tonggak penting dalam perjalanan akademis dan inovasi kami.",
     },
   ],
 };
@@ -58,36 +58,36 @@ export const COMPETITION = {
 export const EXPERIENCES = {
   en: [
     {
-      year: 'January 2024 - December 2025',
-      role: 'Staff of Advocacy and Student Welfare Department',
+      year: "January 2024 - December 2025",
+      role: "Staff of Advocacy and Student Welfare Department",
       company:
         "Badan Eksekutif Mahasiswa Fakultas Ilmu Komputer UPN 'Veteran' Jakarta",
       description: `Bridged communication between students and faculty, empowering student voices and fostering transparency through feedback sessions and advocacy meetings. Helped address key issues such as facility improvements and academic support, strengthening trust and student participation in policy discussions.`,
-      profile_link: 'https://www.instagram.com/p/DFcfH-GTLuc/?img_index=1',
+      profile_link: "https://www.instagram.com/p/DFcfH-GTLuc/?img_index=1",
     },
     {
-      year: 'January 2025 - January 2026',
-      role: 'Vice Chair of Commission II',
+      year: "January 2025 - January 2026",
+      role: "Vice Chair of Commission II",
       company: "Senat Mahasiswa Fakultas Ilmu Komputer UPN 'Veteran' Jakarta",
       description: `Gathered, accommodated, and followed up on student aspirations at the Faculty of Computer Science, UPN Veteran Jakarta. Facilitated communication with the faculty's executive student organization and conducted educational initiatives on advocacy and student rights.`,
-      profile_link: 'https://www.instagram.com/p/DCy4mO7yuyO/?img_index=1',
+      profile_link: "https://www.instagram.com/p/DCy4mO7yuyO/?img_index=1",
     },
   ],
   id: [
     {
-      year: 'Januari 2024 - Desember 2025',
-      role: 'Staf Departemen Advokasi dan Kesejahteraan Mahasiswa',
+      year: "Januari 2024 - Desember 2025",
+      role: "Staf Departemen Advokasi dan Kesejahteraan Mahasiswa",
       company:
         "Badan Eksekutif Mahasiswa Fakultas Ilmu Komputer UPN 'Veteran' Jakarta",
       description: `Membangun komunikasi yang baik antara mahasiswa dan dosen, memberdayakan suara mahasiswa dan mendorong transparansi melalui sesi umpan balik dan pertemuan advokasi. Membantu mengatasi isu-isu penting seperti peningkatan fasilitas dan dukungan akademik, memperkuat kepercayaan dan partisipasi mahasiswa dalam diskusi kebijakan.`,
-      profile_link: 'https://www.instagram.com/p/DFcfH-GTLuc/?img_index=1',
+      profile_link: "https://www.instagram.com/p/DFcfH-GTLuc/?img_index=1",
     },
     {
-      year: 'Januari 2025 - Januari 2026',
-      role: 'Wakil Kepala Komisi II',
+      year: "Januari 2025 - Januari 2026",
+      role: "Wakil Kepala Komisi II",
       company: "Senat Mahasiswa Fakultas Ilmu Komputer UPN 'Veteran' Jakarta",
       description: `Mengumpulkan, mengakomodasi, dan menindaklanjuti aspirasi mahasiswa di Fakultas Ilmu Komputer, UPN Veteran Jakarta. Memfasilitasi komunikasi dengan organisasi mahasiswa eksekutif fakultas dan melakukan inisiatif pendidikan tentang advokasi dan hak-hak mahasiswa.`,
-      profile_link: 'https://www.instagram.com/p/DCy4mO7yuyO/?img_index=1',
+      profile_link: "https://www.instagram.com/p/DCy4mO7yuyO/?img_index=1",
     },
   ],
 };
@@ -95,76 +95,76 @@ export const EXPERIENCES = {
 export const PROJECTS = {
   en: [
     {
-      title: 'Adira.co.id',
+      title: "Adira.co.id",
       image: WebAdira,
       description:
-        'An e-commerce website with features like product listing, shopping cart, and user authentication.',
-      technologies: ['Vue JS', 'Typescript', 'Tailwind CSS'],
-      link: 'https://www.adira.co.id',
+        "Landing page website for PT. Adira Dinamika Multi Finance integrated with a website content management system to organize the layout and content",
+      technologies: ["Vue JS", "Typescript", "Tailwind CSS"],
+      link: "https://www.adira.co.id",
     },
     {
-      title: 'E-Commerce Website',
+      title: "E-Commerce Website",
       image: project1,
       description:
-        'An e-commerce website with features like product listing, shopping cart, and user authentication.',
-      technologies: ['PHP', 'Laravel', 'Bootstrap', 'MySQL'],
-      link: 'https://github.com/TheMekanik/ecommerce-laravel.git',
+        "An e-commerce website with features like product listing, shopping cart, and user authentication.",
+      technologies: ["PHP", "Laravel", "Bootstrap", "MySQL"],
+      link: "https://github.com/TheMekanik/ecommerce-laravel.git",
     },
     {
-      title: 'Scholarship Information System',
+      title: "Scholarship Information System",
       image: project2,
       description:
-        'A website based system that allows students to apply, update profiles, and upload documents, while administrators manage student data, issue recommendations, and review applications.',
-      technologies: ['PHP', 'CSS', 'Laravel', 'MySQL'],
-      link: 'https://github.com/TheMekanik/SistemBeasiswa.git',
+        "A website based system that allows students to apply, update profiles, and upload documents, while administrators manage student data, issue recommendations, and review applications.",
+      technologies: ["PHP", "CSS", "Laravel", "MySQL"],
+      link: "https://github.com/TheMekanik/SistemBeasiswa.git",
     },
     {
-      title: 'Portfolio Website',
+      title: "Portfolio Website",
       image: project3,
       description:
-        'A personal portfolio website showcasing my education, organizational experiences, projects, skills, and contact information.',
-      technologies: ['React', 'Tailwind CSS'],
-      link: '#',
+        "A personal portfolio website showcasing my education, organizational experiences, projects, skills, and contact information.",
+      technologies: ["React", "Tailwind CSS"],
+      link: "#",
     },
   ],
   id: [
     {
-      title: 'Adira.co.id',
+      title: "Adira.co.id",
       image: WebAdira,
       description:
-        'Website landing page untuk PT. Adira Dinamika Multi Finance yang terintegrasi dengan website content management system untuk mengatur tata letak dan isi konten.',
-      technologies: ['Vue JS', 'Typescript', 'Tailwind CSS'],
-      link: 'https://www.adira.co.id',
+        "Website landing page untuk PT. Adira Dinamika Multi Finance yang terintegrasi dengan website content management system untuk mengatur tata letak dan isi konten.",
+      technologies: ["Vue JS", "Typescript", "Tailwind CSS"],
+      link: "https://www.adira.co.id",
     },
     {
-      title: 'Website E-Commerce',
+      title: "Website E-Commerce",
       image: project1,
       description:
-        'Situs web e-commerce dengan fitur-fitur seperti daftar produk, keranjang belanja, dan otentikasi pengguna.',
-      technologies: ['PHP', 'Laravel', 'Bootstrap', 'MySQL'],
-      link: 'https://github.com/TheMekanik/ecommerce-laravel.git',
+        "Situs web e-commerce dengan fitur-fitur seperti daftar produk, keranjang belanja, dan otentikasi pengguna.",
+      technologies: ["PHP", "Laravel", "Bootstrap", "MySQL"],
+      link: "https://github.com/TheMekanik/ecommerce-laravel.git",
     },
     {
-      title: 'Sistem Informasi Manajemen Beasiswa',
+      title: "Sistem Informasi Manajemen Beasiswa",
       image: project2,
       description:
-        'Sebuah sistem berbasis situs web yang memungkinkan siswa untuk mendaftar, memperbarui profil, dan mengunggah dokumen, sementara administrator mengelola data siswa, memberikan rekomendasi, dan meninjau aplikasi.',
-      technologies: ['PHP', 'CSS', 'Laravel', 'MySQL'],
-      link: 'https://github.com/TheMekanik/SistemBeasiswa.git',
+        "Sebuah sistem berbasis situs web yang memungkinkan siswa untuk mendaftar, memperbarui profil, dan mengunggah dokumen, sementara administrator mengelola data siswa, memberikan rekomendasi, dan meninjau aplikasi.",
+      technologies: ["PHP", "CSS", "Laravel", "MySQL"],
+      link: "https://github.com/TheMekanik/SistemBeasiswa.git",
     },
     {
-      title: 'Website Portofolio',
+      title: "Website Portofolio",
       image: project3,
       description:
-        'Situs web portofolio pribadi yang menampilkan pendidikan, pengalaman organisasi, proyek, keterampilan, dan informasi kontak saya.',
-      technologies: ['React', 'Tailwind CSS'],
-      link: '#',
+        "Situs web portofolio pribadi yang menampilkan pendidikan, pengalaman organisasi, proyek, keterampilan, dan informasi kontak saya.",
+      technologies: ["React", "Tailwind CSS"],
+      link: "#",
     },
   ],
 };
 
 export const CONTACT = {
-  address: 'Jakarta, Indonesia ',
-  phoneNo: '+62-815-1456-5145 ',
-  email: 'ahmadroinn24@gmail.com',
+  address: "Jakarta, Indonesia ",
+  phoneNo: "+62-815-1456-5145 ",
+  email: "ahmadroinn24@gmail.com",
 };
